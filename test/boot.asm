09a3e8a8fff77e1983fe27da8b5d2f2c5c8aea30
zhouna <family12345@126.com>
2018-07-12 14:10:55 +0800

    编译/Makefile/bochsrc/运行成功

--- test/boot.asm
+++ test/boot.asm
@@ -1,5 +1,3 @@
-;机器已启动就把这段代码加载到0x7c00处
-;显示一段字符串，然后进入死循环
 org 0x7c00
 
 mov ax, cs
@@ -13,12 +11,12 @@ dispstr:
 	mov ax, 0x1301
 	mov bx, 0x000c
 	mov cx, 10
-	mov dx, 0x00
+	mov dx, 0x1010
 	mov bp, BootMessage
 	int 0x10
 	ret
 
-BootMessage: db 'hello os!', 0
+BootMessage: db "hello os!", 0
 
 times 510-($-$$) db 0
 dw 0xaa55
